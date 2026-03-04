65d5f0133fe465befe82c60a01dd7db91d5a6cb6
antoinechampion <antoine.champion@outlook.fr>
2026-03-04 09:41:55 +0100

    fix: use vcpkg manifest mode to pin Boost 1.84.0 on macOS
    Add vcpkg.json with builtin-baseline from 2024.04.26 (Boost 1.84.0)
    to avoid boost::process v2 breaking changes in Boost 1.86+.
    Simplify configure-mac.sh to just clone vcpkg (manifest mode handles
    package installation). Add LANGUAGES C for JUCE 8 and CMP0144 policy.
    Disable manifest mode on Windows to keep existing SourceForge Boost setup.

--- configure-mac.sh
+++ configure-mac.sh
@@ -5,42 +5,15 @@ set -e
 JuceVersion="8.0.4"
 VCPKGVersion="2026.01.16"
 
-echo "Installing CMake"
-brew install cmake
-brew install ninja
-brew install pkg-config
+echo "Installing dependencies"
+brew install cmake ninja pkg-config
 
 echo "Installing vcpkg"
-git clone https://github.com/microsoft/vcpkg
-cd vcpkg
-git reset --hard "$VCPKGVersion"
-cd ..
+if [[ ! -d "vcpkg" ]]; then
+	git clone --filter=blob:none https://github.com/microsoft/vcpkg.git
+	git -C vcpkg checkout "$VCPKGVersion"
+fi
 ./vcpkg/bootstrap-vcpkg.sh
-
-echo "Installing Boost"
-BOOST_PACKAGES=(
-	"boost-any"
-	"boost-assign"
-	"boost-date-time"
-	"boost-dll"
-	"boost-filesystem"
-	"boost-format"
-	"boost-iostreams"
-	"boost-lexical-cast"
-	"boost-process"
-	"boost-preprocessor"
-	"boost-property-tree"
-	"boost-signals2"
-	"boost-uuid"
-)
-for PACKAGE in $BOOST_PACKAGES; do
-	./vcpkg/vcpkg install $PACKAGE --triplet "x64-osx"
-	./vcpkg/vcpkg install $PACKAGE --triplet "arm64-osx"
-done
-
-echo "Installing cpprestsdk"
-./vcpkg/vcpkg install cpprestsdk --triplet "x64-osx"
-./vcpkg/vcpkg install cpprestsdk --triplet "arm64-osx"
 
 echo "Installing JUCE"
 if [[ ! -d "JUCE" ]]; then
